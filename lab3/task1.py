5d446769d2b2956f14345c2be6abdbc0ee82ff86
kaffe-work <leoo@student.chalmers.se>
2019-11-27 16:34:34 +0100

    almost fix lab 1

--- lab3/task1.py
+++ lab3/task1.py
@@ -1,58 +1,71 @@
 import os
 import math
-import matplotlib.pyplot as plot
 DIR_PATH = "./"
+threshold = 4
+threshold_min = 3.7
 
 def read_data(file_name):
-    Index = 0
+
+    index = 0
     X = 1
     Y = 2
-    Z = 3
-
+    Z = 3    
     parsed_lines = []
     with open(file_name, 'r') as file:
         line = file.readline()
-
-        while "END " not in line:
+        while line:
+            line = line.replace("\t", " ")
             line = list(filter(None, line.split(" ")))
-            if line[DATA_TYPE] == "ATOM" and \
-                line[ATOM_TYPE] == "C" and \
-                    line[ANNOTATED_ATOM_TYPE] == "CA":
-                parsed_lines.append([float(line[Index]),
-                                     float(line[X]),
-                                     float(line[Y]),
-                                     float(line[Z])])
-
+            parsed_lines.append({"index": int(line[index]),"x": float(line[X]),"y": float(line[Y]),"z": float(line[Z])})
             line = file.readline()
     return parsed_lines
 
 def distance(a1, a2):
     """Calculates the euclidian distance between two atoms"""
-    [x1, y1, z1] = a1
-    [x2, y2, z2] = a2
+    return math.sqrt((a1["x"] - a2["x"])**2 + (a1["y"] - a2["y"])**2 + (a1["z"] - a2["z"])**2)
 
-    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
 
-def find_distance(atom, atoms)
+def within_threshold(a1, a2):
+    return ((a1['index']!= a2['index']) and distance(a1,a2) < threshold)
+
+def find_first_atom(atom, atoms):
     "compares an atom to all remainder atoms in the list"
-    neighbours = 0
-    for  j in range(len(atoms))
-        if (distance(atom, atoms[j]) <= 4)
-            neighbours += 1
-    if neighbours == 1 
-        return 1
+    neighbours = []
+    for  atom_i in atoms:
+        if within_threshold(atom, atom_i):
+            neighbours.append(atom_i)
+    return neighbours 
+ 
 
-def find_edge(parsed_lines):
-    for j in range(len(atoms)):
-        nbr_collisions += count_collisions(atoms[j], atoms[j:])
+def find_end(atoms):
+    nbr_collisions = 0 
+    for a in atoms:
+        if len(find_first_atom(a, atoms)) == 1:
+            return a
+
+def find_edge(fixed, rest):
+    if len(rest) < 2:
+        return fixed + rest
+    else:
+        head = fixed[-1]
+        neighbours = find_first_atom(fixed, rest)
+        fixed.append(neighbours)
+        rest = remove_atom(neighbours, rest)
+        find_edge(fixed, rest)
+  
+
+def remove_atom(atom_remove, atoms):
+    return filter(lambda a : a['index'] != edge['index'], atoms)
+
             
 def main():
     file_name = input("Give a path to TXT file:")
-    file_name = os.path.join(DIR_PATH, "./{}.txt"
-                             .format(file_name.upper()))
+    file_name = os.path.join(DIR_PATH, "{}.txt".format(file_name))
     atoms = read_data(file_name)
-    dist_map = distance_map(atoms)
-    plot.show()
+    edge = find_end(atoms)
+    for a in atoms:
+        print(find_end(atoms))
+    sequence = find_edge([edge], remove_atom(edge, atoms))
 
 if __name__ == '__main__':
     main()
